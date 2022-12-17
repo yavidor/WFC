9ceff46b138e9a334a003795cb1ed140b531974b
yavidor <46024219+yavidor@users.noreply.github.com>
2022-12-17 17:29:08 +0200

    init

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import pygame
 class WFC:
-    def __init__(self, pattern_size, cell_size, data):
-        self.pattern_size = pattern_size
-        self.cell_size = cell_size
-        self.data = data
+	def __init__(self, pattern_size, cell_size, data):
+		self.pattern_size = pattern_size
+		self.cell_size = cell_size
+		self.data = data
 
 
 class Cell:
-    def __init__(self, occurrences, neighbors, data):
-        self.occurrences = occurrences
-        self.neighbors = neighbors
-        self.data = data
+	def __init__(self, occurrences, neighbors, data):
+		self.occurrences = occurrences
+		self.neighbors = neighbors
+		self.data = data
 
 
 print("Hello")
