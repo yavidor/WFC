4dde76e1e3189f5be964c87b7c2846e3fb46d799
yavidor <46024219+yavidor@users.noreply.github.com>
2022-12-17 17:02:22 +0200

    init

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pygame
+class WFC:
+    def __init__(self, pattern_size, cell_size, data):
+        self.pattern_size = pattern_size
+        self.cell_size = cell_size
+        self.data = data
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Cell:
+    def __init__(self, occurrences, neighbors, data):
+        self.occurrences = occurrences
+        self.neighbors = neighbors
+        self.data = data
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+print("Hello")
