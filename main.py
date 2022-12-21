bb17cb788e771ee605471a7ec5f279c6eb1389c4
yavidor <46024219+yavidor@users.noreply.github.com>
2022-12-21 11:28:09 +0200

    Basic classes

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
+from typing import Tuple
+
 import pygame
-
-
-class WFC:
-	def __init__(self, pattern_size, cell_size, data):
-		self.pattern_size = pattern_size
-		self.cell_size = cell_size
-		self.data = data
+from pygame import Surface, SurfaceType
 
 
 class Cell:
@@ -15,4 +11,30 @@ class Cell:
 		self.data = data
 
 
-print("Hello")
+class WFC:
+	screen: Surface | SurfaceType
+	pattern: Surface | SurfaceType
+	pattern_size: tuple[int, int]
+	cell_size: int
+
+	def __init__(self, path, cell_size, output_size):
+		self.path = path
+		self.pattern = pygame.image.load(path)
+		self.pattern_size = self.pattern.get_size()
+		self.screen = pygame.display.set_mode(output_size)
+		self.cell_size = cell_size
+
+
+pygame.init()
+wfc = WFC('Rooms.png', 3, (120, 120))
+wfc.screen.blit(wfc.pattern, (0, 0))
+print(wfc.screen.get_size())
+pygame.display.flip()
+running = True
+while running:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+
+# Quit Pygame
+pygame.quit()
