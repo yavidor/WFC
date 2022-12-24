0bcfa0a246799ff9d58678fd7a4d4a770ca5d120
yavidor <46024219+yavidor@users.noreply.github.com>
2022-12-24 14:09:03 +0200

    Trying to be better with types

--- main.py
+++ main.py
@@ -12,17 +12,16 @@ class Cell:
 
 
 class WFC:
+	cell_size: int
 	screen: Surface | SurfaceType
 	pattern: Surface | SurfaceType
 	pattern_size: tuple[int, int]
-	cell_size: int
 
-	def __init__(self, path, cell_size, output_size):
-		self.path = path
+	def __init__(self, path: str, cell_size: int, output_size: tuple[int, int]):
+		self.cell_size = cell_size
 		self.pattern = pygame.image.load(path)
 		self.pattern_size = self.pattern.get_size()
 		self.screen = pygame.display.set_mode(output_size)
-		self.cell_size = cell_size
 
 
 pygame.init()
