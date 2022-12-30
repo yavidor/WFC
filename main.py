73689c179e7d0f06260071cbf1a6f404ef2d6da5
yavidor <46024219+yavidor@users.noreply.github.com>
2022-12-30 19:50:06 +0200

    The WFC class was useless

--- main.py
+++ main.py
@@ -28,53 +28,11 @@ class Cell:
 
 	def __str__(self):
 		return f"data: {self.data}\noccurrences: {self.occurrences}\nUp Wall: {self.up_wall}\nDown Wall: {self.down_wall}\n" \
-			f"Left Wall: {self.left_wall}\nRight Wall: {self.right_wall} "
+	f"Left Wall: {self.left_wall}\nRight Wall: {self.right_wall} "
 
 
 def occ(thing):
 	return thing.occurrences
-
-
-class WFC:
-	cell_size: int
-	screen: Surface | SurfaceType
-	input_image: Surface | SurfaceType
-	input_size: tuple[int, int]
-	patterns_raw: list[list[list[int]]]
-	patterns_cells: list[Cell]
-	pixels: pygame.PixelArray
-
-	def __init__(self, path: str, cell_size: int, output_size: tuple[int, int]):
-		self.cell_size = cell_size
-		self.input_image = pygame.image.load(path)
-		self.input_size = self.input_image.get_size()
-		self.screen = pygame.display.set_mode(output_size)
-		self.patterns_raw = []
-		self.patterns_cells = []
-		self.patterns_dict = {}
-		self.pixels = pygame.PixelArray(self.input_image)
-		print(self.pixels)
-		for i in range(0, self.input_size[0] - cell_size):
-			for j in range(0, self.input_size[1] - cell_size):
-				self.patterns_raw.append([])
-				for k in range(0, cell_size):
-					self.patterns_raw[-1].append([])
-					for m in range(0, cell_size):
-						self.patterns_raw[-1][-1].append(self.pixels[j + m][i + k])
-				add_to_dic(self.patterns_dict, self.patterns_raw[-1])
-		print(self.patterns_raw)
-		print(len(self.patterns_raw))
-		self.patterns_raw = [el for index, el in enumerate(self.patterns_raw) if el not in self.patterns_raw[:index]]
-		print(len(self.patterns_raw))
-		print(self.patterns_dict)
-		for pattern in self.patterns_raw:
-			self.patterns_cells.append(Cell(pattern, self.patterns_dict[str(pattern)]))
-		# print(self.patterns_cells[0])
-		self.patterns_cells.sort(key=occ, reverse=True)
-		# print(self.patterns_cells[0])
-		for cell in self.patterns_cells:
-			print(cell)
-		self.pixels.close()
 
 
 def add_to_dic(dictionary, key):
@@ -84,10 +42,37 @@ def add_to_dic(dictionary, key):
 		dictionary[str(key)] = 1
 
 
+cell_size: int = 3
+path: str = "Rooms.png"
+output_size: tuple[int, int] = (120, 120)
+input_image: Surface | SurfaceType = pygame.image.load(path)
+input_size: tuple[int, int] = input_image.get_size()
+screen: Surface | SurfaceType = pygame.display.set_mode(output_size)
+patterns_raw: list[list[list[int]]] = []
+patterns_cells: list[Cell] = []
+patterns_dict = {}
+pixels: pygame.PixelArray = pygame.PixelArray(input_image)
+print(pixels)
+for i in range(0, input_size[0] - cell_size):
+	for j in range(0, input_size[1] - cell_size):
+		patterns_raw.append([])
+		for k in range(0, cell_size):
+			patterns_raw[-1].append([])
+			for m in range(0, cell_size):
+				patterns_raw[-1][-1].append(pixels[j + m][i + k])
+		add_to_dic(patterns_dict, patterns_raw[-1])
+print(patterns_raw)
+print(len(patterns_raw))
+patterns_raw = [el for index, el in enumerate(patterns_raw) if el not in patterns_raw[:index]]
+print(len(patterns_raw))
+print(patterns_dict)
+for pattern in patterns_raw:
+	patterns_cells.append(Cell(pattern, patterns_dict[str(pattern)]))
+	patterns_cells.sort(key=occ, reverse=True)
+for cell in patterns_cells:
+	print(cell)
+pixels.close()
 pygame.init()
-wfc = WFC('Rooms.png', 3, (120, 120))
-wfc.screen.blit(wfc.input_image, (0, 0))
-print(wfc.screen.get_size())
 pygame.display.flip()
 running = True
 while running:
