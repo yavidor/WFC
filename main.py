f508aaa9449c311b9ce32a1f6ad035c10adfe275
yavidor <46024219+yavidor@users.noreply.github.com>
2022-12-31 17:30:00 +0200

    Added step 1 and 2 of the algorithm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
+import random
+
 import pygame
 from pygame import Surface, SurfaceType
 
@@ -40,28 +42,50 @@ class Cell:
 		self.patterns = patterns
 		self.x = x
 		self.y = y
+		self.collapsed = False
+		self.final_pattern = None
+
+	def __str__(self):
+		return f"patterns: {self.patterns}\nx: {self.x}\ny: {self.y}"
 
 
 def occ(thing):
 	return thing.occurrences
 
 
-def calculate_entropy(possibilities: list[Pattern]):
-	print(len(possibilities))
+def calculate_entropy(possibilities: list[Pattern]) -> float:
 	weights = sum(p.occurrences for p in possibilities)
-	print(weights)
 	entropy = math.log(weights, len(possibilities)) - (
 			sum(p.occurrences * math.log(p.occurrences, len(possibilities)) for p in possibilities) / weights)
 	return entropy
 
 
-def add_to_dic(dictionary, key):
+def add_to_dic(dictionary, key) -> None:
 	if str(key) in dictionary:
 		dictionary[str(key)] += 1
 	else:
 		dictionary[str(key)] = 1
 
 
+def find_lowest_entropy(cells: list[list[Cell]]) -> Cell:
+	lowest = cells[0][0]
+	print(lowest)
+	for i in range(len(cells)):
+		for cell in cells[i]:
+			lowest_entropy = calculate_entropy(lowest.patterns)
+			cell_entropy = calculate_entropy(cell.patterns)
+			if (lowest_entropy == cell_entropy and random.random() > 0.5) or cell_entropy < lowest_entropy:
+				lowest = cell
+	return lowest
+
+
+def collapse(cell: Cell):
+	cell.collapsed = True
+	cell.final_pattern = random.choice(cell.patterns)
+	cell.patterns = []
+
+
+# Declaring variables
 cell_size: int = 3
 path: str = "Rooms.png"
 output_size: tuple[int, int] = (120, 120)
@@ -72,7 +96,7 @@ patterns_raw: list[list[list[int]]] = []
 patterns_cells: list[Pattern] = []
 patterns_dict = {}
 pixels: pygame.PixelArray = pygame.PixelArray(input_image)
-print(pixels)
+# Creating the patterns
 for i in range(0, input_size[0] - cell_size):
 	for j in range(0, input_size[1] - cell_size):
 		patterns_raw.append([])
@@ -81,23 +105,19 @@ for i in range(0, input_size[0] - cell_size):
 			for m in range(0, cell_size):
 				patterns_raw[-1][-1].append(pixels[j + m][i + k])
 		add_to_dic(patterns_dict, patterns_raw[-1])
-print(patterns_raw)
-print(len(patterns_raw))
-patterns_raw = [el for index, el in enumerate(patterns_raw) if el not in patterns_raw[:index]]
-print(len(patterns_raw))
-print(patterns_dict)
+patterns_raw = [element for index, element in enumerate(patterns_raw) if element not in patterns_raw[:index]]
 for pattern in patterns_raw:
 	patterns_cells.append(Pattern(pattern, patterns_dict[str(pattern)]))
 	patterns_cells.sort(key=occ, reverse=True)
-output_cells: list[list[list[Pattern]]] = []
+output_cells: list[list[Cell]] = []
 for i in range(0, int(output_size[0] / 3)):
 	output_cells.append([])
 	for j in range(0, int(output_size[1] / 3)):
-		output_cells[-1].append([])
-		output_cells[-1][-1].extend(patterns_cells)
+		output_cells[-1].append(Cell(patterns_cells, i, j))
 
 pixels.close()
 # pygame.init()
 print(calculate_entropy(patterns_cells))
+print(find_lowest_entropy(output_cells))
 # pygame.image.save(screen, './pics/hello.jpeg')
 # pygame.quit()
