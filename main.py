0e09603e0f12a8a3ca596f7149ba01002b655007
yavidor <46024219+yavidor@users.noreply.github.com>
2022-12-31 15:23:10 +0200

    Turned Cell to Pattern and created new Cell class to represent a cell in the output

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 from pygame import Surface, SurfaceType
 
 
-class Cell:
+class Pattern:
 	data: list[list[int]]
 	occurrences: int
 	up_wall: list[int]
@@ -31,16 +31,27 @@ class Cell:
 		       f"Left Wall: {self.left_wall}\nRight Wall: {self.right_wall}"
 
 
+class Cell:
+	patterns: list[Pattern]
+	x: int
+	y: int
+
+	def __init__(self, patterns: list[Pattern], x: int, y: int):
+		self.patterns = patterns
+		self.x = x
+		self.y = y
+
+
 def occ(thing):
 	return thing.occurrences
 
 
-def calculate_entropy(possibilities: list[Cell]):
+def calculate_entropy(possibilities: list[Pattern]):
 	print(len(possibilities))
 	weights = sum(p.occurrences for p in possibilities)
 	print(weights)
 	entropy = math.log(weights, len(possibilities)) - (
-				sum(p.occurrences * math.log(p.occurrences, len(possibilities)) for p in possibilities) / weights)
+			sum(p.occurrences * math.log(p.occurrences, len(possibilities)) for p in possibilities) / weights)
 	return entropy
 
 
@@ -58,7 +69,7 @@ input_image: Surface | SurfaceType = pygame.image.load(path)
 input_size: tuple[int, int] = input_image.get_size()
 screen: Surface | SurfaceType = pygame.display.set_mode(output_size)
 patterns_raw: list[list[list[int]]] = []
-patterns_cells: list[Cell] = []
+patterns_cells: list[Pattern] = []
 patterns_dict = {}
 pixels: pygame.PixelArray = pygame.PixelArray(input_image)
 print(pixels)
@@ -76,9 +87,9 @@ patterns_raw = [el for index, el in enumerate(patterns_raw) if el not in pattern
 print(len(patterns_raw))
 print(patterns_dict)
 for pattern in patterns_raw:
-	patterns_cells.append(Cell(pattern, patterns_dict[str(pattern)]))
+	patterns_cells.append(Pattern(pattern, patterns_dict[str(pattern)]))
 	patterns_cells.sort(key=occ, reverse=True)
-output_cells: list[list[list[Cell]]] = []
+output_cells: list[list[list[Pattern]]] = []
 for i in range(0, int(output_size[0] / 3)):
 	output_cells.append([])
 	for j in range(0, int(output_size[1] / 3)):
